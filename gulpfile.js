ba0f44c6ea124f09c7391b157ad0708aca68128d
Chris Stead <cmstead@gmail.com>
2017-11-03 15:05:02 -0700

    Added initial files

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const istanbul = require('gulp-istanbul');
+const mocha = require('gulp-mocha');
+
+const sourceFiles = [
+    'bin/**/*.js',
+    'index.js',
+    '!node_modules/**'
+];
+
+const testFiles = [
+    'test/**/*.js'
+];
+
+gulp.task('lint', () => {
+    return gulp.src(sourceFiles)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('pre-test', function () {
+    return gulp.src(sourceFiles)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['lint', 'pre-test'], function () {
+    gulp.src(testFiles, { read: false })
+        .pipe(mocha())
+        .pipe(istanbul.writeReports({ reporters: ['text-summary'] }))
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 80 } }));
+});
+
+gulp.task('test');
